16f28458da53f40c4216e9331f1facf7b48aced3
shubhamchauhan4880 <41935411+shubhamchauhan4880@users.noreply.github.com>
2022-04-11 16:19:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Trantor!\n'
+  const msg = 'Hello Trantor shubham this side!\n'
   res.end(msg);
 });
 
